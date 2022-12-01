94bf8727115df182c52f4a625956db057ea81ea9
sergblin <blinofftash86@mail.ru>
2022-12-01 16:28:22 +0300

    commit zad2

---
+++ zad2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите цикл, который принимает на вход два числа A и B и возводит число A в натуральную степень B.
+3, 5 -> 243 
+2, 4 -> 16 */
+
+int Exponentiation(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    return result;
+}
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int exponentiation = Exponentiation(numberA, numberB);
+Console.WriteLine($"Число A в натуральной степень B равно: {exponentiation}");
+
