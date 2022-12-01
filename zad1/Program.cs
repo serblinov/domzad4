58f20a7ead238c037533667c51a628c8e1a8ca85
sergblin <blinofftash86@mail.ru>
2022-12-01 16:24:57 +0300

    commit zad1

---
+++ zad1/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int SumNumber(int number)
+{
+int countOfDigit = Convert.ToString(number).Length;
+int nextNumber = 0;
+int result = 0;
+for (int i = 0; i < countOfDigit; i++) 
+ {
+    nextNumber = number - number % 10;
+    result = result + (number - nextNumber);
+    number = number / 10;
+ }return result;
+  }
+int sumNumber = SumNumber(number);
+Console.WriteLine($"Сумма цифр в числе равна: {sumNumber}");
+
